60f6b9f85b3fc35739778e226a9db8906a364403
Kevin Scott <kevin@hitheory.com>
2019-01-26 15:57:05 -0500

    Update paths to templates

--- src/templates/interactive/components.js
+++ src/templates/interactive/components.js
@@ -1,11 +1,18 @@
 import styled from 'react-emotion';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Body = styled.div`
   flex: 1;
   display: flex;
   height: 100%;
   background: black;
   position: relative;
+  flex: 1;
 
   &:after {
     content: "Loading";
--- src/templates/interactive/index.js
+++ src/templates/interactive/index.js
@@ -1,60 +1,158 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
-import MDXRenderer from 'gatsby-mdx/mdx-renderer';
-import { MDXProvider } from "@mdx-js/tag";
+// import MDXRenderer from 'gatsby-mdx/mdx-renderer';
+// import { MDXProvider } from "@mdx-js/tag";
 import {
   Header,
+  Body,
   Container,
 } from './components';
 
 /* eslint-disable */
-const Body = ({ sandbox, body, mdxScope }) => {
-  if (sandbox) {
-    const qs = [
-      'autoresize=1',
-      'expanddevtools=0',
-      'hidenavigation=1',
-    ];
-    const src = `https://codesandbox.io/embed/${sandbox}?${qs.join('&')}`;
-    const options = [
-      'allow-modals',
-      'allow-forms',
-      'allow-popups',
-      'allow-scripts',
-      'allow-same-origin',
-    ].join(' ');
+const Sandbox = ({ sandbox }) => {
+  const qs = [
+    'autoresize=1',
+    'expanddevtools=0',
+    'hidenavigation=1',
+  ];
+  const src = `https://codesandbox.io/s/${sandbox}?${qs.join('&')}`;
+  const options = [
+    'allow-top-navigation',
+    'allow-modals',
+    'allow-forms',
+    'allow-popups',
+    'allow-scripts',
+    'allow-same-origin',
+  ].join(' ');
 
-    return (
-      <iframe
-        src={src}
-        sandbox={options}
-      />
-    );
+  return (
+    <iframe
+      src={src}
+      sandbox={options}
+    />
+  );
+};
+
+const createSandbox = () => {
+  const code = `console.log('hii');`;
+  const html = '<div id="root"></div>';
+
+  return fetch("https://codesandbox.io/api/v1/sandboxes/define?json=1", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      files: {
+        "package.json": {
+          content: {
+            dependencies: {
+              react: "latest",
+              "react-dom": "latest"
+            }
+          }
+        },
+        "index.js": {
+          content: code
+        },
+        "index.html": {
+          content: html
+        }
+      }
+    })
+  }).then(x => x.json()).then(data => data.sandbox_id);
+}
+
+const forkSandbox = () => {
+  return fetch('https://cors-anywhere.herokuapp.com/https://codesandbox.io/api/v1/sandboxes/movkjqoq88/fork', {
+    method: 'POST',
+    headers: new Headers({
+      'Origin': 'localhost:8000',
+      'X-Requested-With': 'localhost:8000',
+    }),
+  }).then(r => r.json()).then(r => {
+    console.log(r);
+    return r.data.id;
+  });
+};
+
+const USE_CACHE = false;
+
+const getSandbox = () => {
+  try {
+    if (USE_CACHE) {
+      // const sandboxId = '4q47307rvx';
+      const sandboxId = localStorage[window.location.pathname];
+      if (sandboxId) {
+        console.log('sandbox id saved to local storage', sandboxId);
+        return Promise.resolve(sandboxId);
+      }
+    }
+  } catch(err) { }
+
+  console.log('create sandbox from scratch');
+  return forkSandbox().then(sandboxId => {
+    if (sandboxId) {
+      console.log('sandboxId', sandboxId);
+      localStorage[window.location.pathname] = sandboxId;
+    }
+    return sandboxId;
+  });
+}
+
+class PageTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sandboxId: null,
+    };
   }
 
-  return (
-    <MDXRenderer scope={mdxScope}>
-      {body}
-    </MDXRenderer>
-  );
-};
+  componentDidMount() {
+    const {
+      data: {
+        mdx: {
+          fields: {
+            sandbox,
+          },
+        },
+      },
+    } = this.props;
 
-const PageTemplate = ({ data, __mdxScope, ...rest }) => {
-  const post = data.mdx;
-  const {
-    sandbox,
-  } = post.fields;
-  return (
-    <MDXProvider components={{}}>
-      <Header>
-        Deep Learning With Javascript: A Hacker's Guide to Getting Started With Neural Networks
-      </Header>
+    window.location = `https://codesandbox.io/s/github/dljsbook/templates/tree/master/${sandbox}`;
+    // getSandbox().then(sandboxId => {
+    //   this.setState({
+    //     sandboxId,
+    //   });
+    // });
+  }
+
+  render() {
+    const {
+      data,
+      __mdxScope,
+      ...rest
+    } = this.props;
+    const post = data.mdx;
+    const {
+      sandbox,
+    } = post.fields;
+    return (
       <Container>
-        <Body sandbox={sandbox} mdxScope={__mdxScope} body={post.code.body} />
+        <Header>
+          Deep Learning With Javascript: A Hacker's Guide to Getting Started With Neural Networks
+        </Header>
+        <Body>
+          {this.state.sandboxId && (
+            <Sandbox sandbox={this.state.sandboxId} mdxScope={__mdxScope} />
+          )}
+        </Body>
       </Container>
-    </MDXProvider>
-  );
-};
+    );
+  }
+}
 
 export default PageTemplate;
 
