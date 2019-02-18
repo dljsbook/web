28179052c7536781b57199bfce6536d4fe73e5b0
Kevin Scott <kevin@hitheory.com>
2019-02-18 14:07:35 -0500

    Fix links

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+        siteUrl: 'https://dljsbook.com'
       }
     },
     {
