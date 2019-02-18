9e12cf2c7dab85e4660ff04888379ad941f79972
Kevin Scott <kevin@hitheory.com>
2019-02-18 15:19:46 -0500

    Try tf

--- src/utils/getOptions.js
+++ src/utils/getOptions.js
@@ -1,5 +1,5 @@
-/*
 import * as tf from "@tensorflow/tfjs";
+/*
 // import * as tfvis from '@tensorflow/tfjs-vis';
 
 const url = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
