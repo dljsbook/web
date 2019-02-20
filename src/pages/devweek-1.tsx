54bb8d3c03e665ba1aa75af57a83ddceb31f7bb7
Kevin Scott <kevin@hitheory.com>
2019-02-20 07:53:15 -0500

    Fix page

--- src/pages/devweek-1.tsx
+++ src/pages/devweek-1.tsx
@@ -7,6 +7,11 @@ import styled from 'react-emotion';
 const Body = styled.div`
   width: 100%;
   padding-right: 30px;
+
+  img {
+    max-width: 300px;
+    margin: 0 auto;
+  }
 `;
 
 class DevWeekOne extends React.Component {
@@ -16,7 +21,12 @@ class DevWeekOne extends React.Component {
         <Page>
           <Container>
             <Body>
-              hi
+              <p>Here are slides for my talk at Developer Week, February 20th, 2019:</p>
+              <p><a href="https://www.dropbox.com/s/il187u01dyqp9se/KevinScott-2_20_19-ask_what_ai_can_do_for_you.key?dl=0">Ask What AI Can Do For You</a></p>
+              <p>If you&quot;re looking for my other talk, <a href="/devweek-2">you can find it here</a>.</p>
+              <hr />
+              <p>If you&quot;re interested on learning how to build a Neural Network in Javascript, <a href="/" target="_blank">check out the book</a>!</p>
+              <p><a href="/" target="_blank"><img src="https://dljsbook.com/dljscover.png" alt="Deep Learning With Javascript" title="Deep Learning With Javascript" /></a></p>
             </Body>
           </Container>
         </Page>
