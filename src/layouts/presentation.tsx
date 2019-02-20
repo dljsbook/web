b7c768d18498e790bce38bc19c56cc1e0a0d0a0d
Kevin Scott <kevin@hitheory.com>
2019-02-20 09:03:33 -0500

    Simplify mdx

--- src/layouts/presentation.tsx
+++ src/layouts/presentation.tsx
@@ -27,63 +27,16 @@ const Body = styled.div`
   }
 `;
 
-type StaticQueryProps = {
-  site: {
-    siteMetadata: {
-      title: string;
-      description: string;
-    };
-  };
-};
-
 const PresentationLayout: React.SFC = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query IndexLayoutQuery {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-        allMdx(sort: { fields: [fields___date], order: DESC }) {
-          edges {
-            node {
-              excerpt
-              fields {
-                slug
-                date(formatString: "DD MMMM, YYYY")
-                title
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data: StaticQueryProps) => (
-      <LayoutRoot>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: data.site.siteMetadata.description },
-            { name: 'keywords', content: 'gatsbyjs, gatsby, javascript, sample, something' }
-          ]}
-        />
-        <Header title={data.site.siteMetadata.title} />
-        <LayoutMain>
-          <IndexLayout>
-            <Page>
-              <Container>
-                <Body>
-                  {children}
-                </Body>
-              </Container>
-            </Page>
-          </IndexLayout>
-        </LayoutMain>
-      </LayoutRoot>
-    )}
-  />
+  <IndexLayout>
+    <Page>
+      <Container>
+        <Body>
+          {children}
+        </Body>
+      </Container>
+    </Page>
+  </IndexLayout>
 );
 
 export default PresentationLayout;
