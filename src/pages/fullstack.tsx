c421e567041c1f4d529aaade71bfe17dad906cde
Kevin Scott <kevin@hitheory.com>
2019-05-16 19:28:27 -0400

    Add fullstack pdf

--- src/pages/fullstack.tsx
+++ src/pages/fullstack.tsx
@@ -4,7 +4,7 @@ import PresentationLayout from '../layouts/presentation';
 const FullStack = () => (
   <PresentationLayout>
     <p>Here are slides for my talk May 17th, 2019:</p>
-    <p><a target="_blank" href="https://www.dropbox.com/s/ptj3y807tz7osth/Ask%20What%20AI%20Can%20Do%20For%20You.key?dl=0">Ask What AI Can Do For You</a></p>
+    <p><a target="_blank" href="/fullstack.pdf">Ask What AI Can Do For You</a></p>
     <hr />
     <p>If you're interested on learning how to build a Neural Network in Javascript, <a href="/" target="_blank">check out the book</a>!</p>
     <p><a href="/" target="_blank"><img src="https://dljsbook.com/dljscover.png" alt="Deep Learning in Javascript" title="Deep Learning in Javascript" /></a></p>
