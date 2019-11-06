e7636bff735bd9f17d48753bd9fc9582cd3a9c94
Kevin Scott <kevin@hitheory.com>
2019-11-06 06:53:45 -0600

    Add dev week austi

---
+++ src/pages/devweek-austin.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PresentationLayout from '../layouts/presentation';
+
+const Slides = () => (
+  <PresentationLayout>
+    <p>Here are slides for my talk November 6th, 2019:</p>
+    <p><a target="_blank" href="/fullstack.pdf">Ask What AI Can Do For You</a></p>
+    <hr />
+    <p>If you're interested on learning how to build a Neural Network in Javascript, <a href="/" target="_blank">check out the book</a>!</p>
+    <p><a href="/" target="_blank"><img src="https://dljsbook.com/dljscover.png" alt="Deep Learning in Javascript" title="Deep Learning in Javascript" /></a></p>
+    <hr />
+    <p><strong>Additional Resources</strong></p>
+    <p>Here are links to the Codepens:</p>
+    <ul>
+      <li><a target="_blank" href="https://codepen.io/thekevinscott/pen/Jxameq">A Simple Neural Network</a></li>
+      <li><a target="_blank" href="https://codepen.io/thekevinscott/project/editor/DvGbxP">Transfer Learning for Images</a></li>
+    </ul>
+    <p>Information on hardware performance improvements <a target="_blank" href="https://heartbeat.fritz.ai/ios-12-core-ml-benchmarks-b7a79811aac1">can be found on Fritz.AI's blog</a>.</p>
+    <p><a target="_blank" href="https://cs.nyu.edu/~fergus/papers/zeilerECCV2014.pdf">Link to the paper <em>Visualizing and Understanding Convolutional Networks</em> by Matthew D. Zeiler and Rob Fergus can be found here</a>. It explores how different layers of a Network learn visual features, vital in enabling Transfer Learning.</p>
+    <hr />
+    <p>Finally, I always appreciate feedback! <a href="https://twitter.com/thekevinscott" target="_blank">Shoot me a message on Twitter</a> and let me know what you thought.</p>
+    <p>Thanks! &mdash; Kevin</p>
+  </PresentationLayout>
+);
+
+export default Slides;
